504d7f7299fe04344d0137c62df80b126521470e
AnandTheertha <anandtheerthav3@gmail.com>
2023-02-23 14:53:40 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from './pages/home/Home';
+import './App.css';
+import Auth from './pages/Auth/Auth';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <div className="blur" style={{top: '-18%', right: '0'}}></div> */}
+        <Home />
+        {/* <Auth /> */}
+        {/* <div className="blur" style={{top: '36%', left: '-8rem'}}></div> */}
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/components/TrendCard/TrendCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './TrendCard.css'
+
+import { TrendData } from '../../Data/TrendData'
+
+const TrendCard = () => {
+  return (
+    <div className="TrendCard">
+        {TrendData.map( (trend) => {
+            return(
+                <div className="trend">
+                   <span>#{trend.name}</span>
+                    <span>{trend.shares}k shares</span>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default TrendCard
